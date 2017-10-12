5170d1a7ee59f7c7d82c2981c9f1a5fd487d1844
Enrico Seiler <enrico.seiler@hotmail.de>
2017-10-12 17:17:08 +0200

    WIP: compile yara from source

--- recipes/yara/build.sh
+++ recipes/yara/build.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
+set -e
+
+cd $SRC_DIR
+mkdir yara_build
+cd yara_build
+export PATH_BACKUP=$PATH
+if [ $ARCH = "64" ]; then
+	env -i PATH="$PATH" CXX="$CXX" cmake .. -DSEQAN_BUILD_SYSTEM=APP:yara -DSEQAN_STATIC_APPS=1 -DSEQAN_ARCH_SSE4=1 -DCMAKE_CXX_COMPILER=$CXX
+else
+	env -i PATH="$PATH" CXX="$CXX" cmake .. -DSEQAN_BUILD_SYSTEM=APP:yara -DSEQAN_STATIC_APPS=1 -DCMAKE_CXX_FLAGS="-m32" -DCMAKE_CXX_COMPILER=$CXX
+fi	
+export PATH=$PATH_BACKUP
+cd bin
+binaries="\
+yara_mapper \
+yara_indexer \
+"
 
 BINARY_HOME=$PREFIX/bin
 PKG_HOME=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
 
 cd $SRC_DIR/bin
 
-binaries="\
-yara_mapper \
-yara_indexer \
-"
 mkdir -p $PREFIX/bin
 mkdir -p $PKG_HOME
 
