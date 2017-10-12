92ed5acc8fed1eaa021ff7cc2e4f93d8ee3d3003
Enrico Seiler <enrico.seiler@hotmail.de>
2017-10-12 19:27:13 +0200

    builds locally

--- recipes/yara/build.sh
+++ recipes/yara/build.sh
@@ -5,13 +5,14 @@ cd $SRC_DIR
 mkdir yara_build
 cd yara_build
 export PATH_BACKUP=$PATH
-if [ $ARCH = "64" ]; then
+if (( $ARCH == 64 )) ; then
 	env -i PATH="$PATH" CXX="$CXX" cmake .. -DSEQAN_BUILD_SYSTEM=APP:yara -DSEQAN_STATIC_APPS=1 -DSEQAN_ARCH_SSE4=1 -DCMAKE_CXX_COMPILER=$CXX
 else
 	env -i PATH="$PATH" CXX="$CXX" cmake .. -DSEQAN_BUILD_SYSTEM=APP:yara -DSEQAN_STATIC_APPS=1 -DCMAKE_CXX_FLAGS="-m32" -DCMAKE_CXX_COMPILER=$CXX
 fi	
+make all
 export PATH=$PATH_BACKUP
-cd bin
+
 binaries="\
 yara_mapper \
 yara_indexer \
@@ -20,9 +21,7 @@ yara_indexer \
 BINARY_HOME=$PREFIX/bin
 PKG_HOME=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
 
-cd $SRC_DIR/bin
-
 mkdir -p $PREFIX/bin
 mkdir -p $PKG_HOME
 
-for i in $binaries; do cp $SRC_DIR/bin/$i $PKG_HOME/$i && chmod a+x $PKG_HOME/$i && ln -s $PKG_HOME/$i $BINARY_HOME/$i; done
+for i in $binaries; do cp $SRC_DIR/yara_build/bin/$i $PKG_HOME/$i && chmod a+x $PKG_HOME/$i && ln -s $PKG_HOME/$i $BINARY_HOME/$i; done
