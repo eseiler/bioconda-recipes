1a546ed81e5964aafb3b2dac350c09b05307d51b
Enrico Seiler <enrico.seiler@hotmail.de>
2017-10-13 10:12:11 +0200

    Yara 0.9.9

--- recipes/yara/build.sh
+++ recipes/yara/build.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
+set -eu -o pipefail
 
 BINARY_HOME=$PREFIX/bin
 PKG_HOME=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
 
-cd $SRC_DIR/bin
+if [[ "$(uname -s)" == "Linux"* ]]; then
+	if grep flags /proc/cpuinfo | grep sse4 > /dev/null; then
+		INSTRUCTION=sse4
+	else
+		INSTRUCTION=x64
+	fi
+else
+	if sysctl -a | grep machdep.cpu.features | grep SSE4 > /dev/null; then
+		INSTRUCTION=sse4
+	else
+		INSTRUCTION=x64
+	fi
+fi
+
+cd $SRC_DIR/$INSTRUCTION/bin
 
 binaries="\
 yara_mapper \
 yara_indexer \
 "
+
 mkdir -p $PREFIX/bin
 mkdir -p $PKG_HOME
 
