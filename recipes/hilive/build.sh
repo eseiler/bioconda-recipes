9c2c0e8b67c189c4e1caf665919b5cd38b96ea2b
eseiler <enrico.seiler@hotmail.de>
2017-10-27 15:33:45 +0200

    hilive-test

---
+++ recipes/hilive/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sed -i 's/set (CMAKE_MODULE_PATH/# set (CMAKE_MODULE_PATH/' CMakeLists.txt
+sed -i 's/set (SEQAN_INCLUDE_PATH "\/usr\/local\/lib\/seqan\/include\/")/set (SEQAN_INCLUDE_PATH "'"$PREFIX"'\/include\/seqan\/include")/' CMakeLists.txt
+mkdir build 
+cd build
+cmake ..
+make
+
+mkdir -p $PREFIX/bin
+cp $SRC_DIR/build/hilive-build $PREFIX/bin/
+cp $SRC_DIR/build/hilive $PREFIX/bin/
+cp $SRC_DIR/build/hilive-out $PREFIX/bin/
+
+chmod +x $PREFIX/bin/hilive*
