953745a8378af2bb97b6c911e56dd3429d80d096
Enrico Seiler <eseiler@users.noreply.github.com>
2017-10-27 16:12:13 +0200

    Update build.sh

--- recipes/hilive/build.sh
+++ recipes/hilive/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sed -i 's/set (CMAKE_MODULE_PATH/# set (CMAKE_MODULE_PATH/' CMakeLists.txt
-sed -i 's/set (SEQAN_INCLUDE_PATH "\/usr\/local\/lib\/seqan\/include\/")/set (SEQAN_INCLUDE_PATH "'$(sed 's=/=\\/=g' <<< $PREFIX)'\/include\/seqan\/include")/' CMakeLists.txt
+sed -i 's/set (SEQAN_INCLUDE_PATH "\/usr\/local\/lib\/seqan\/include\/")/set (SEQAN_INCLUDE_PATH "'$(sed 's=/=\\/=g' <<< $PREFIX)'\/include\/seqan")/' CMakeLists.txt
 mkdir build 
 cd build
 cmake ..
